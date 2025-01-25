477fefd1846c83710119ae4769628df814a6b6a3
Selim Tekin <tekinselim.57.57@gmail.com>
2025-01-26 00:01:57 +0300

    API Weather

--- Project6_ApiWeather/Entities/City.cs
+++ Project6_ApiWeather/Entities/City.cs
@@ -3,7 +3,7 @@
 	public class City
 	{
         public int CityId { get; set; }
-		public string CityName { get; set; }
+	    public string CityName { get; set; }
         public string Country { get; set; }
         public decimal Temp { get; set; }
         public string Detail { get; set; }
