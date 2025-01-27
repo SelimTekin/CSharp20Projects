6f02ad975d44d4d54ec465f6540c3691f9520711
Selim Tekin <tekinselim.57.57@gmail.com>
2025-01-27 17:12:34 +0300

    Mogo DB Order

---
+++ Project9_MongoDbOrder/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Project9_MongoDbOrder
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtId = new System.Windows.Forms.TextBox();
+			this.btnList = new System.Windows.Forms.Button();
+			this.btnCreate = new System.Windows.Forms.Button();
+			this.txtCustomer = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnDelete = new System.Windows.Forms.Button();
+			this.txtDistrict = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.txtCity = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.btnGetById = new System.Windows.Forms.Button();
+			this.txtTotalPrice = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label1.Location = new System.Drawing.Point(33, 29);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(89, 22);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Sipariş Id:";
+			// 
+			// txtId
+			// 
+			this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.txtId.Location = new System.Drawing.Point(128, 26);
+			this.txtId.Name = "txtId";
+			this.txtId.Size = new System.Drawing.Size(243, 28);
+			this.txtId.TabIndex = 1;
+			// 
+			// btnList
+			// 
+			this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnList.Location = new System.Drawing.Point(392, 25);
+			this.btnList.Name = "btnList";
+			this.btnList.Size = new System.Drawing.Size(134, 29);
+			this.btnList.TabIndex = 2;
+			this.btnList.Text = "Listele";
+			this.btnList.UseVisualStyleBackColor = true;
+			this.btnList.Click += new System.EventHandler(this.btnList_Click);
+			// 
+			// btnCreate
+			// 
+			this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnCreate.Location = new System.Drawing.Point(392, 60);
+			this.btnCreate.Name = "btnCreate";
+			this.btnCreate.Size = new System.Drawing.Size(134, 29);
+			this.btnCreate.TabIndex = 5;
+			this.btnCreate.Text = "Ekle";
+			this.btnCreate.UseVisualStyleBackColor = true;
+			this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+			// 
+			// txtCustomer
+			// 
+			this.txtCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.txtCustomer.Location = new System.Drawing.Point(128, 61);
+			this.txtCustomer.Name = "txtCustomer";
+			this.txtCustomer.Size = new System.Drawing.Size(243, 28);
+			this.txtCustomer.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label2.Location = new System.Drawing.Point(18, 64);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(104, 22);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Müşteri Adı:";
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnDelete.Location = new System.Drawing.Point(392, 95);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(134, 29);
+			this.btnDelete.TabIndex = 8;
+			this.btnDelete.Text = "Sil";
+			this.btnDelete.UseVisualStyleBackColor = true;
+			this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+			// 
+			// txtDistrict
+			// 
+			this.txtDistrict.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.txtDistrict.Location = new System.Drawing.Point(128, 96);
+			this.txtDistrict.Name = "txtDistrict";
+			this.txtDistrict.Size = new System.Drawing.Size(243, 28);
+			this.txtDistrict.TabIndex = 7;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.Location = new System.Drawing.Point(80, 99);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(42, 22);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "İlçe:";
+			// 
+			// btnUpdate
+			// 
+			this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnUpdate.Location = new System.Drawing.Point(392, 130);
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.Size = new System.Drawing.Size(134, 29);
+			this.btnUpdate.TabIndex = 11;
+			this.btnUpdate.Text = "Güncelle";
+			this.btnUpdate.UseVisualStyleBackColor = true;
+			this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+			// 
+			// txtCity
+			// 
+			this.txtCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.txtCity.Location = new System.Drawing.Point(128, 131);
+			this.txtCity.Name = "txtCity";
+			this.txtCity.Size = new System.Drawing.Size(243, 28);
+			this.txtCity.TabIndex = 10;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label4.Location = new System.Drawing.Point(65, 134);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(57, 22);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Şehir:";
+			// 
+			// btnGetById
+			// 
+			this.btnGetById.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnGetById.Location = new System.Drawing.Point(392, 165);
+			this.btnGetById.Name = "btnGetById";
+			this.btnGetById.Size = new System.Drawing.Size(134, 29);
+			this.btnGetById.TabIndex = 14;
+			this.btnGetById.Text = "Getir";
+			this.btnGetById.UseVisualStyleBackColor = true;
+			this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
+			// 
+			// txtTotalPrice
+			// 
+			this.txtTotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.txtTotalPrice.Location = new System.Drawing.Point(128, 166);
+			this.txtTotalPrice.Name = "txtTotalPrice";
+			this.txtTotalPrice.Size = new System.Drawing.Size(243, 28);
+			this.txtTotalPrice.TabIndex = 13;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label5.Location = new System.Drawing.Point(3, 169);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(119, 22);
+			this.label5.TabIndex = 12;
+			this.label5.Text = "Toplam Fiyat:";
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(7, 227);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(781, 211);
+			this.dataGridView1.TabIndex = 15;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(557, 25);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(231, 169);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 16;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.btnGetById);
+			this.Controls.Add(this.txtTotalPrice);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.txtCity);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.txtDistrict);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.btnCreate);
+			this.Controls.Add(this.txtCustomer);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.btnList);
+			this.Controls.Add(this.txtId);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtId;
+		private System.Windows.Forms.Button btnList;
+		private System.Windows.Forms.Button btnCreate;
+		private System.Windows.Forms.TextBox txtCustomer;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnDelete;
+		private System.Windows.Forms.TextBox txtDistrict;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btnUpdate;
+		private System.Windows.Forms.TextBox txtCity;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnGetById;
+		private System.Windows.Forms.TextBox txtTotalPrice;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ Project9_MongoDbOrder/Services/MongoDbConnection.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project9_MongoDbOrder.Services
+{
+	public class MongoDbConnection
+	{
+		private IMongoDatabase _database;
+
+		public MongoDbConnection()
+		{
+			var client = new MongoClient("mongodb://localhost:27017");
+			_database = client.GetDatabase("Db9ProejctOrder");
+		}
+		// Bson -> json gibi mongo db için bir tür
+		public IMongoCollection<BsonDocument> GetOrdersCollection()
+		{
+			return _database.GetCollection<BsonDocument>("Orders");
+		}
+	}
+}
